a9b84a4036577a6f800d4ccc5a7437483e95bf76
liza31337 <liza31337@e08d0fb5-4147-0410-a205-bba44f1f51a3>
2008-10-26 18:51:18 +0000

    Branch for search development

---
+++ branches/bookworm-search/library/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from models import EpubArchive, BookAuthor,HTMLFile,StylesheetFile, ImageFile, UserPref
+
+
+class EpubArchiveAdmin(admin.ModelAdmin):
+    fields=('title','name','owner','orderable_author','toc', 'opf')
+    list_filter=('orderable_author',)
+    search_fields = ['title']
+    ordering = ('title', '-created_time')
+    list_per_page = 500
+
+class BookAuthorAdmin(admin.ModelAdmin):
+    ordering = ('name',)
+
+class HTMLFileAdmin(admin.ModelAdmin):
+    fields=('title','filename','processed_content','is_read')
+    ordering = ('title','path')
+
+class StylesheetFileAdmin(admin.ModelAdmin):
+    fields=('filename','path')
+
+class ImageFileAdmin(admin.ModelAdmin):    
+    fields=('filename','path', 'content_type')
+
+class UserPrefAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(EpubArchive, EpubArchiveAdmin)
+admin.site.register(BookAuthor, BookAuthorAdmin)
+admin.site.register(HTMLFile, HTMLFileAdmin)
+admin.site.register(StylesheetFile, StylesheetFileAdmin)
+admin.site.register(ImageFile, ImageFileAdmin)
+admin.site.register(UserPref, UserPrefAdmin)
+
+
